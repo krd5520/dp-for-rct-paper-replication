7ed6e88e24f4247fc954ca8181f1bde5aee51360
krd5520 <kdowden621@gmail.com>
2026-02-02 13:49:13 -0500

    remove extraneous

--- global-libraries.R
+++ global-libraries.R
@@ -43,13 +43,28 @@ pkgTest <- function(x)
 	return("OK")
 }
 
-global.libraries <- c("dplyr","devtools","rprojroot","tidyr","ggplot2","knitr","kableExtra",
-                      "sandwhich")
+global.libraries <- c("dplyr", #version 1.1.4
+                      "devtools", #version 2.4.6
+                      "rprojroot", # version 2.1.1
+                      "ggplot2", #version 4.0.1
+                      "RColorBrewer", #version 1.1-3
+                      "knitr", #version 1.51
+                      "kableExtra", #version 1.4.0
+                      "sandwich", #version 3.1-1
+                      "markdown", #version 2.0
+                      "haven", #version 2.5.5
+                      "labelled" #version 2.16.0
+)
 
 results <- sapply(as.list(global.libraries), pkgTest)
+cbind(libraries,results)
 
-if (!require("grateful",character.only = TRUE)) {
-  # install.packages("remotes")
-  remotes::install_github("Pakillo/grateful")
-  if(!require("grateful",character.only = TRUE)) stop("Package not found")
-}
+# installing an additional package, version used in analysis
+remotes::install_github("krd5520/DPrct/tree/660e72c62ddcca17e6cdda29712bc8b34c745651") #https://github.com/krd5520/DPrct
+
+
+# if (!require("grateful",character.only = TRUE)) {
+#   # install.packages("remotes")
+#   remotes::install_github("Pakillo/grateful")
+#   if(!require("grateful",character.only = TRUE)) stop("Package not found")
+# }
